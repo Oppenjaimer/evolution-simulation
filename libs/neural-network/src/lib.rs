2de1458f0c4bb88d0172bdc17683d4da8aaa624e
Jaime Redondo <jairegra@gmail.com>
2025-10-25 18:28:11 +0200

    Implement baseline network randomness and tests

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,3 +1,9 @@
+use rand::{Rng, RngCore};
+
+/* -------------------------------------------------------------------------- */
+/*                               IMPLEMENTATION                               */
+/* -------------------------------------------------------------------------- */
+
 /* --------------------------------- Neuron --------------------------------- */
 
 #[derive(Debug)]
@@ -7,10 +13,20 @@ struct Neuron {
 }
 
 impl Neuron {
+    fn random(rng: &mut dyn RngCore, input_size: usize) -> Self {
+        let bias = rng.gen_range(-1.0..=1.0);
+
+        let weights = (0..input_size)
+            .map(|_| rng.gen_range(-1.0..=1.0))
+            .collect();
+
+        Self { bias, weights }
+    }
+
     fn propagate(&self, inputs: &[f32]) -> f32 {
         assert_eq!(inputs.len(), self.weights.len());
 
-        let mut output = inputs
+        let output = inputs
             .iter()
             .zip(&self.weights)
             .map(|(input, weight)| input * weight)
@@ -29,12 +45,25 @@ struct Layer {
 }
 
 impl Layer {
+    fn random(rng: &mut dyn RngCore, input_size: usize, output_size: usize) -> Self {
+        let neurons = (0..output_size)
+            .map(|_| Neuron::random(rng, input_size))
+            .collect();
+
+        Self { neurons }
+    }
+
     fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
         self.neurons
             .iter()
             .map(|neuron| neuron.propagate(&inputs))
             .collect()
     }
+}
+
+#[derive(Debug)]
+pub struct LayerTopology {
+    pub neurons: usize,
 }
 
 /* --------------------------------- Network -------------------------------- */
@@ -44,9 +73,61 @@ pub struct Network {
 }
 
 impl Network {
+    pub fn random(rng: &mut dyn RngCore, layers: &[LayerTopology]) -> Self {
+        let layers = layers
+            .windows(2)
+            .map(|layers| Layer::random(rng, layers[0].neurons, layers[1].neurons))
+            .collect();
+
+        Self { layers }
+    }
+
     pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
         self.layers
             .iter()
             .fold(inputs, |inputs, layer| layer.propagate(inputs))
+    }
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                    TESTS                                   */
+/* -------------------------------------------------------------------------- */
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use approx::assert_relative_eq;
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha8Rng;
+
+    #[test]
+    fn random() {
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+        let neuron = Neuron::random(&mut rng, 4);
+
+        assert_relative_eq!(neuron.bias, -0.6255188);
+
+        assert_relative_eq!(
+            neuron.weights.as_slice(),
+            [0.67383957, 0.8181262, 0.26284897, 0.5238807].as_ref()
+        );
+    }
+
+    #[test]
+    fn propagate() {
+        let neuron = Neuron {
+            bias: 0.5,
+            weights: vec![-0.3, 0.8],
+        };
+
+        assert_relative_eq!(
+            neuron.propagate(&[-10.0, -10.0]),
+            0.0,
+        );
+
+        assert_relative_eq!(
+            neuron.propagate(&[0.5, 1.0]),
+            (-0.3 * 0.5) + (0.8 * 1.0) + 0.5,
+        );
     }
 }
